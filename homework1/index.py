70101f98c2b0e243e2e3948540e6bbc271dcb134
VakhtangTsintsadze777 <vaxo19902010@gmail.com>
2025-01-20 13:15:07 +0400

    main

---
+++ homework1/index.py
@@ -0,0 +1,40 @@
+# N1
+first_name = "vakho"
+last_name = "tsintsadze"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+print(type(full_name))
+
+
+# N2
+age = 20
+sentence = "you are " + str(age) + " years old"
+print(sentence)
+print(type(age))
+
+
+# N3
+temperature = 20.3
+sentence2 = "the temperature is " + str(temperature) + " degrees"
+print(sentence2)
+print(type(temperature))
+
+
+# N4
+its_hot = False
+
+if temperature >= 30:
+    its_hot = True
+
+print(type(its_hot))
+
+# N5
+
+# so 'int' means integer it is any z number, 'float' numbers are q numbers they include '.' , 'str' - string are letters and everything that is between " " s, 'bool' - boolean it can be true or false
+# exp:
+int = 5
+float = 4.3
+str = "string"
+bool = False
--- homework1/main.py
+++ homework1/main.py
@@ -1,7 +1,35 @@
-def main() -> None:
-    # Write your solution here
-    pass
+# 1
+print('Hello World!')
 
+# 2
+print(5 + 7)
 
-if __name__ == '__main__':
-    main()
+# 3
+print(37 - 8)
+
+# 4
+print(-90 * 4)
+
+# 5
+print(100 / 3)
+
+# 6
+print(0.2 + 0.1)
+
+# 7
+print(7.068 * 4.3)
+
+# 8
+print(8 ** 2)
+
+# 9
+print(9 ** 3)
+
+# 10
+print(5 / 3)
+
+# 11
+print(5 // 3)
+
+# 12
+print(99 % 7)
